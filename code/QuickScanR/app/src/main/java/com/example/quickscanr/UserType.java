5791ba9165e8ce275972658d589c3d3b8828b60e
Tammy Young <tammy.q.young@gmail.com>
2024-02-23 01:47:36 -0700

    added user types

---
+++ code/QuickScanR/app/src/main/java/com/example/quickscanr/UserType.java
@@ -0,0 +1,7 @@
+package com.example.quickscanr;
+
+public class UserType {
+    public final static Integer ATTENDEE = 0;
+    public final static Integer ORGANIZER = 1;
+    public final static Integer ADMIN = 2;
+}
