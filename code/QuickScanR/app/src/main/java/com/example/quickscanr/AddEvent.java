dc7edd9f0138ef5bd24d66a9d47a5f1d2fcace50
Tammy Young <tammy.q.young@gmail.com>
2024-02-24 00:15:56 -0700

    forgot this in the last push

--- code/QuickScanR/app/src/main/java/com/example/quickscanr/AddEvent.java
+++ code/QuickScanR/app/src/main/java/com/example/quickscanr/AddEvent.java
@@ -1,4 +1,30 @@
 package com.example.quickscanr;
 
-public class AddEvent {
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AddEvent extends InnerPageFragment {
+
+    public AddEvent() {}
+
+    public static AddEvent newInstance(String param1, String param2) {
+        AddEvent fragment = new AddEvent();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.add_event, container, false);
+        addButtonListeners(getActivity(), v);
+        return v;
+    }
 }
