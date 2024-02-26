2fb5bf46973784a6fced7001111802c59237abd3
Tammy Young <tammy.q.young@gmail.com>
2024-02-26 15:12:20 -0700

    event dashboard and details functionality

--- code/QuickScanR/app/src/main/java/com/example/quickscanr/EventDashboard.java
+++ code/QuickScanR/app/src/main/java/com/example/quickscanr/EventDashboard.java
@@ -1,4 +1,44 @@
 package com.example.quickscanr;
 
-public class EventDashboard {
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.io.Serializable;
+
+public class EventDashboard extends InnerPageFragment {
+
+    private static final String EVENT = "event";
+
+    private Event event;
+
+    public EventDashboard() {}
+
+    public static EventDashboard newInstance(Event event) {
+        EventDashboard fragment = new EventDashboard();
+        Bundle args = new Bundle();
+        args.putSerializable(EVENT, event);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            event = (Event) getArguments().getSerializable(EVENT);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v =  inflater.inflate(R.layout.event_dashboard, container, false);
+        addButtonListeners(getActivity(), v, new OrganizerEventList());
+        return v;
+    }
 }
--- code/QuickScanR/app/src/main/java/com/example/quickscanr/EventDetails.java
+++ code/QuickScanR/app/src/main/java/com/example/quickscanr/EventDetails.java
@@ -1,4 +1,41 @@
 package com.example.quickscanr;
 
-public class EventDetails {
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+public class EventDetails extends InnerPageFragment {
+
+    private static final String EVENT = "event";
+
+    private Event event;
+
+    public EventDetails() {}
+
+    public static EventDetails newInstance(Event event) {
+        EventDetails fragment = new EventDetails();
+        Bundle args = new Bundle();
+        args.putSerializable(EVENT, event);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            event = (Event) getArguments().getSerializable(EVENT);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.event_details, container, false);
+        addButtonListeners(getActivity(), v, new AttendeeEventList());
+        return v;
+    }
 }
