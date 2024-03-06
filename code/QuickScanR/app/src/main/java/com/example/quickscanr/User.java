2151c1c9e81d7b08e672a47210a91a9df3f1ad05
ananyashukla2 <ashukla2@ualberta.ca>
2024-03-06 11:32:33 -0700

    "checked in attendee list"

--- code/QuickScanR/app/src/main/java/com/example/quickscanr/User.java
+++ code/QuickScanR/app/src/main/java/com/example/quickscanr/User.java
@@ -1,13 +1,19 @@
 package com.example.quickscanr;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 import java.io.Serializable;
 
 public class User implements Serializable {
 
     private String name;
+    private String homepage;
     private String phoneNumber;
     private String email;
     private int userType;
+    private String userId;
+
+    public User() {}
 
     public User(String name, String phoneNumber, String email, int userType) {
         this.name = name;
@@ -22,6 +28,14 @@ public class User implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getHomepage() {
+        return homepage;
+    }
+
+    public void setHomepage(String homepage) {
+        this.homepage = homepage;
     }
 
     public String getPhoneNumber() {
@@ -46,5 +60,27 @@ public class User implements Serializable {
 
     public void setUserType(int userType) {
         this.userType = userType;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    /**
+     * for editing a profile. checks the fields for valid input
+     * @param nameField the input box for a user's name
+     * @return a boolean saying if there was errors or not
+     */
+    public boolean isErrors(TextInputEditText nameField) {
+        boolean wasErrors = false;
+        if (nameField.getText().toString() == "") {
+            nameField.setError("User must have a name!");
+            wasErrors = true;
+        }
+        return wasErrors;
     }
 }
