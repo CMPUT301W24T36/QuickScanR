a6a975108aed276ca71c35548771a21300a28c1d
Tammy Young <tammy.q.young@gmail.com>
2024-03-08 03:55:52 -0700

    add espresso and junit to gradle

--- code/QuickScanR/app/build.gradle.kts
+++ code/QuickScanR/app/build.gradle.kts
@@ -33,6 +33,9 @@ android {
 }
 
 dependencies {
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.google.zxing:core:3.4.1")
     implementation("com.journeyapps:zxing-android-embedded:4.3.0")
     implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
