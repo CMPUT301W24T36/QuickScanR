ebee7e66b239cafca72087f9fcea4c569268aa19
Tammy Young <tammy.q.young@gmail.com>
2024-03-08 15:08:26 -0700

    added more navigation tests for organizers

--- code/QuickScanR/app/src/androidTest/java/com/example/quickscanr/OrganizerNavigationTest.java
+++ code/QuickScanR/app/src/androidTest/java/com/example/quickscanr/OrganizerNavigationTest.java
@@ -6,6 +6,7 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 import androidx.fragment.app.FragmentActivity;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -76,10 +77,68 @@ public class OrganizerNavigationTest {
      * click the add event button and check that it's being showed
      */
     @Test
-    public void testOrganizerNav(){
+    public void testAddEventBtn(){
         goHome();
         onView(withId(R.id.nav_o_add_event_btn)).perform(click());
         onView(withId(R.id.add_event_page)).check(matches(ViewMatchers.isDisplayed()));
+    }
+
+    /**
+     * click on organizer event list
+     * click on first item in the list
+     * check that the event dashboard page is being shown
+     */
+    @Test
+    public void testEventDashBoard() {
+        goHome();
+        onView(withId(R.id.nav_o_events_btn)).perform(click());
+        onView(withId(R.id.org_ev_list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        onView(withId(R.id.event_dashboard_page)).check(matches(ViewMatchers.isDisplayed()));
+    }
+
+    /**
+     * go to organizer event list
+     * click on first item in the list
+     * click on the # checked-in text
+     * check that the checked-in attendees list is shown
+     */
+    @Test
+    public void testCheckedInAttendeeListAccess() {
+        goHome();
+        onView(withId(R.id.nav_o_events_btn)).perform(click());
+        onView(withId(R.id.org_ev_list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        onView(withId(R.id.evdash_txt_stat4)).perform(click());
+        onView(withId(R.id.checked_in_attendees_list)).check(matches(ViewMatchers.isDisplayed()));
+    }
+
+    /**
+     * go to organizer event list
+     * click on first item in the list
+     * click on the check-in button
+     * check that the check in qr code is being shown
+     */
+    @Test
+    public void testCheckInQRAccess() {
+        goHome();
+        onView(withId(R.id.nav_o_events_btn)).perform(click());
+        onView(withId(R.id.org_ev_list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        onView(withId(R.id.evdash_btn_checkin)).perform(click());
+        onView(withId(R.id.check_in_qr_page)).check(matches(ViewMatchers.isDisplayed()));
+    }
+
+    /**
+     * go to organizer event list
+     * click on first item in the list
+     * click on the qr code button (for the promotion qr code)
+     * check that the promotion qr code is being shown
+     */
+    @Test
+    public void testPromotionInQRAccess() {
+        goHome();
+        onView(withId(R.id.nav_o_events_btn)).perform(click());
+        onView(withId(R.id.org_ev_list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        onView(withId(R.id.evdash_btn_qrcode)).perform(click());
+        onView(withId(R.id.promotional_qr_code_page)).check(matches(ViewMatchers.isDisplayed()));
     }
 
 }
