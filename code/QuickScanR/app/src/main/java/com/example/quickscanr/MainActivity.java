7b315b78eb6a1b4984c327609b47cb796f2dfd96
mahughes23 <122180767+mahughes23@users.noreply.github.com>
2024-04-06 22:33:00 -0600

    Minor permissions change

--- code/QuickScanR/app/src/main/java/com/example/quickscanr/MainActivity.java
+++ code/QuickScanR/app/src/main/java/com/example/quickscanr/MainActivity.java
@@ -61,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
             // If permission already been granted
             initializeApp();
         }
+        initializeApp();
     }
 
     /**
