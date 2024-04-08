2cce561f6afab02ea12a87231a6f478f4a6f5f14
ananyashukla2 <ashukla2@ualberta.ca>
2024-04-08 01:31:32 -0600

    has animation error

--- code/QuickScanR/app/src/androidTest/java/com/example/quickscanr/AttendeeListTest.java
+++ code/QuickScanR/app/src/androidTest/java/com/example/quickscanr/AttendeeListTest.java
@@ -4,20 +4,21 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
-import static org.hamcrest.Matchers.containsString;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 
-import android.net.Uri;
-import android.os.Build;
 import android.util.Log;
+
 
 import androidx.fragment.app.FragmentManager;
 import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.ViewAssertion;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
+import androidx.annotation.NonNull;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.uiautomator.By;
 import androidx.test.uiautomator.UiDevice;
@@ -28,10 +29,15 @@ import androidx.test.uiautomator.UiSelector;
 import androidx.test.uiautomator.Until;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import org.hamcrest.Matcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -54,9 +60,7 @@ public class AttendeeListTest {
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-    private static String testEventId;
-    User user;
-    MainActivity mActivity;
+    private String testEventId = "testEvent";
 
     private static void allowPermissionsIfNeeded() {
         UiDevice device = UiDevice.getInstance(getInstrumentation());
@@ -76,11 +80,11 @@ public class AttendeeListTest {
 
     @Before
     public void setUp() {
-        // set proper user type
+        // only setup once
         if (!userSet) {
             allowPermissionsIfNeeded();
             try {
-                Thread.sleep(5000L);    // let database setup
+                Thread.sleep(2000L);    // let database setup
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
@@ -93,75 +97,78 @@ public class AttendeeListTest {
                 } catch (Exception e1) {
                     try {
                         onView(withId(R.id.nav_a_profile_btn)).perform(click());
-                    } catch (Exception e2) {}
+                    } catch (Exception e2) {
+                    }
                 }
             }
-            // edit current user type to be attendee
+            // edit current user type to be organizer
             onView(withId(R.id.user_edit_profile)).perform(click());
             onView(withId(R.id.edit_profile_usertype)).perform(click());
-            onView(withText(UserType.getString(UserType.ATTENDEE))).perform(click());
+            onView(withText(UserType.getString(UserType.ORGANIZER))).perform(click());
             onView(withId(R.id.save_profile_btn)).perform(click());
-            onView(withId(R.id.nav_a_announcements_btn)).perform(click());
             userSet = true;
         }
-
-        // get fragment instance + go to page
-        onView(withId(R.id.nav_a_profile_btn)).perform(click());
-        onView(withId(R.id.user_edit_profile)).perform(click());
-        scenario.getScenario().onActivity(activity -> {
-            fragManager = activity.getSupportFragmentManager();
-            fragManager.executePendingTransactions();
-            EditProfile editFragment = (EditProfile) fragManager.findFragmentByTag("EDIT_PROFILE");
-            assertNotNull(editFragment);
-            editInstance = editFragment;
-            user = activity.user;
-            mActivity = activity;
-        });
-
-        // wait to ensure activity callback
         try {
-            Thread.sleep(500L);
+            Thread.sleep(2000L);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+
+        addTestEvent();
+
     }
 
+    public void addTestEvent() {
+        // Test event data
+        Map<String, Object> eventData = new HashMap<>();
+        eventData.put("name", "Test Event For Attendee List");
+        eventData.put("description", "Event Description");
+        eventData.put("location", "Location");
+        eventData.put("start_date", "26-03-2024");
+        eventData.put("end_date", "26-03-2024");
+        eventData.put("restrictions", "Restrictions");
+        eventData.put("timestamp", System.currentTimeMillis());
+        eventData.put("posterID", "default");
+        eventData.put("ownerID", MainActivity.user.getUserId());
+        eventData.put("LocationName", "Los Pollos Hermanos, Isleta Boulevard Southwest, Albuquerque, NM, USA");
+        eventData.put("maxAttendees", "-1");
+        List<String> signedUpUsers = Arrays.asList("user123", "user222", "user333");
+        eventData.put("signedUpUsers", signedUpUsers);
 
-    @After
-    public void tearDown() {
-        // Ensure testEventId is not null before attempting to delete the document
-        if (testEventId != null) {
-            // Delete test event from database
-            db.collection("events").document(testEventId).delete();
-        }
-    }
+        DocumentReference eventRef = db.collection("events").document(testEventId);
 
-    public void addTestEvent(final OnEventAddedListener listener) {
-        // test event data
-        Map<String, Object> data = new HashMap<>();
-        data.put(DatabaseConstants.evNameKey, "Test Event");
-        data.put(DatabaseConstants.evDescKey, "Event Description");
-        data.put(DatabaseConstants.evLocNameKey, "Location");
-        data.put(DatabaseConstants.evStartKey, "26-03-2024");
-        data.put(DatabaseConstants.evEndKey, "26-03-2024");
-        data.put(DatabaseConstants.evRestricKey, "");
-        data.put(DatabaseConstants.evPosterKey, "default");
-        data.put(DatabaseConstants.evOwnerKey, MainActivity.user.getUserId());
-
-        db.collection(DatabaseConstants.eventColName).add(data)
-                .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+        eventRef.set(eventData)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
-                    public void onComplete(@NonNull Task<DocumentReference> task) {
+                    public void onComplete(@NonNull Task<Void> task) {
                         if (task.isSuccessful()) {
-                            DocumentReference documentReference = task.getResult();
-                            if (documentReference != null) {
-                                String eventId = documentReference.getId();
-                                // Notify the listener that the event has been added
-                                listener.onEventAdded(eventId);
-                            }
+                            Log.d("AttendeeListTest", "Event added successfully with custom ID: " + testEventId);
                         } else {
-                            // Handle error
-                            Log.e("AttendeeListTest", "Error adding test event", task.getException());
+                            Log.e("AttendeeListTest", "Failed to add event with custom ID", task.getException());
+                        }
+                    }
+                });
+    }
+
+    public void checkInAttendee(String testEventId) {
+        Map<String, Object> attendeeData = new HashMap<>();
+        attendeeData.put("name", "John Doe");
+        List<Long> timestamps = Arrays.asList(1712517089266L, 1712443722028L);
+        attendeeData.put("timestamps", timestamps);
+
+        DocumentReference attendeeRef = db.collection("events")
+                .document(testEventId)
+                .collection("attendees")
+                .document("user123"); // Set the document ID here
+
+        attendeeRef.set(attendeeData)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("AttendeeListTest", "Attendee added successfully with ID: user123");
+                        } else {
+                            Log.e("AttendeeListTest", "Failed to add attendee with ID: user123", task.getException());
                         }
                     }
                 });
@@ -169,26 +176,62 @@ public class AttendeeListTest {
 
     @Test
     public void testAttendeeListAfterCheckIn() {
-        // Simulate checking in an attendee
-        checkInAttendee();
+
+        checkInAttendee(testEventId);
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        onView(withId(R.id.nav_o_events_btn)).perform(click());
+
+        onView(withId(R.id.organizer_event_list)).check(matches(isDisplayed()));
+        onView(withText("Test Event For Attendee List")).check(matches(isDisplayed())).perform(click());
+
+        onView(withId(R.id.event_dashboard_page)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.evdash_img_stat4)).perform(click());
+
+        onView(withId(R.id.chkd_usrs_list)).check(matches(isDisplayed()));
 
         // Verify that the attendee appears on the checked-in attendees list
         onView(withId(R.id.chkd_usr_text)).check(matches(withText("John Doe")));
-        // Dynamically verify the count of timestamps
-        onView(withId(R.id.chkd_usr_count)).check(matches(withText("2"))); // Assuming "2" is the count of timestamps
+        onView(withId(R.id.chkd_usr_count)).check(matches(withText("2")));
     }
 
-    public void checkInAttendee() {
-        // Simulate checking in an attendee
-        Map<String, Object> attendeeData = new HashMap<>();
-        attendeeData.put("name", "John Doe");
-        List<Long> timestamps = Arrays.asList(1712517089266L, 1712443722028L);
-        attendeeData.put("timestamps", timestamps);
-
-        db.collection("events").document(testEventId).collection("attendees").add(attendeeData);
+    @After
+    public void tearDown() {
+        db.collection("events").document(testEventId).collection("attendees").get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        // Iterate through the query results
+                        for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                            // Get the reference to the document and delete it
+                            db.collection("events").document(testEventId).collection("attendees")
+                                    .document(documentSnapshot.getId()).delete()
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                Log.d("AttendeeListTest", "Attendee document deleted successfully");
+                                            } else {
+                                                Log.e("AttendeeListTest", "Failed to delete attendee document", task.getException());
+                                            }
+                                        }
+                                    });
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e("AttendeeListTest", "Error getting attendee documents", e);
+                    }
+                });
     }
 
-    public interface OnEventAddedListener {
-        void onEventAdded(String eventId);
-    }
 }
+
